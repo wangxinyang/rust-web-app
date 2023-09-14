1621f61c3c68638735409c9c2ef516833c0a4ccb
Tosei <359387050@qq.com>
2023-09-14 18:09:18 +0900

    add task unit test src

--- src/_dev_utils/mod.rs
+++ src/_dev_utils/mod.rs
@@ -1,6 +1,8 @@
 use tokio::sync::OnceCell;
 use tracing::info;
 
+use crate::model::ModelManager;
+
 mod dev_db;
 
 /// Initialize environment for local development
@@ -13,4 +15,15 @@ pub async fn init_dev() {
         dev_db::init_dev_db().await.unwrap();
     })
     .await;
+}
+
+pub async fn init_test() -> ModelManager {
+    static INIT: OnceCell<ModelManager> = OnceCell::const_new();
+
+    INIT.get_or_init(|| async {
+        init_dev().await;
+        ModelManager::new().await.unwrap()
+    })
+    .await
+    .clone()
 }
--- src/model/task.rs
+++ src/model/task.rs
@@ -20,7 +20,7 @@ pub struct TaskUpdate {
 pub struct TaskBmc;
 
 impl TaskBmc {
-    pub async fn create(_ctx: Ctx, mm: ModelManager, task_c: TaskForCreate) -> Result<i64> {
+    pub async fn create(_ctx: Ctx, mm: &ModelManager, task_c: TaskForCreate) -> Result<i64> {
         let db = mm.db();
 
         // query_as返回一个QueryAs的结构体，
@@ -51,8 +51,50 @@ impl TaskBmc {
         let (id,) = sqlx::query_as("INSERT INTO tasks (title) VALUES ($1) RETURNING id")
             .bind(task_c.title)
             .fetch_one(db)
-            .await?;
+            .await?; // 在这里可以使用？的原因是在model::Error中已经实现了From<sqlx::Error>
 
         Ok(id)
     }
 }
+
+// region:    ---Tests
+#[cfg(test)]
+mod tests {
+    #![allow(unused)]
+    use crate::_dev_utils;
+
+    use super::*;
+    use anyhow::Result;
+
+    #[tokio::test]
+    async fn test_create_ok() -> Result<()> {
+        // Setup && Features
+        let mm = _dev_utils::init_test().await;
+        let ctx = Ctx::root_ctx();
+        let fx_title = "test_create_ok Title";
+
+        // --Exec
+        let task_c = TaskForCreate {
+            title: fx_title.to_string(),
+        };
+        let id = TaskBmc::create(ctx, &mm, task_c).await?;
+
+        // Check
+        let (title,): (String,) = sqlx::query_as("SELECT title FROM tasks WHERE id = $1")
+            .bind(id)
+            .fetch_one(mm.db())
+            .await?;
+        assert_eq!(title, fx_title);
+
+        // --Cleanup
+        let count = sqlx::query("DELETE FROM tasks WHERE id = $1")
+            .bind(id)
+            .execute(mm.db())
+            .await?
+            .rows_affected();
+        assert_eq!(count, 1);
+
+        Ok(())
+    }
+}
+// endregion: ---Tests
